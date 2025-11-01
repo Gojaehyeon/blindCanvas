0a79659198d16c6ce7eaa8b2c79b1961c036bb04
gojaehyeon <gojaehyeonstudio@gmail.com>
2025-11-01 13:36:58 +0900

    feat: 프로젝트 생성

---
+++ pidgin/pidgin/pidginApp.swift
@@ -0,0 +1,17 @@
+//
+//  pidginApp.swift
+//  pidgin
+//
+//  Created by go on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct pidginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
